828d66b6c7ff0521da3e29f2b7ac88129407c0f8
Smilla Due <smilla.due@live.com>
2026-01-12 15:21:23 +0100

    fixing pytests so they skip when preprocessed data doesn't exist yet

--- tests/test_data.py
+++ tests/test_data.py
@@ -1,8 +1,12 @@
+import os.path
+
+import pytest
+import torch
+from mlops_project.data import MyDataset
 from torch.utils.data import Dataset
 
-from mlops_project.data import MyDataset
 
-
+@pytest.mark.skipif(not os.path.exists("data/processed/train_images.pt"), reason="Data files not found")
 def test_my_dataset():
     """Test the MyDataset class."""
     dataset = MyDataset("data/raw")
