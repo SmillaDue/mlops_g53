2bff86f6d3700c6931344f2b27157e4aecc7b1f7
TP <t.pock@campus.lmu.de>
2026-01-15 16:52:13 +0100

    pushed image to gcp and started smoke test on gcp

--- scripts/submit_vertex.sh
+++ scripts/submit_vertex.sh
@@ -7,4 +7,6 @@ REGION=europe-west1
 gcloud ai custom-jobs create \
   --project="$PROJECT_ID" \
   --region="$REGION" \
+  --display-name="mlops-training-smoketest-$(date +%Y%m%d-%H%M%S)" \
   --config="gcp/vertex_job.yaml"
+  
